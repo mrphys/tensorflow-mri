94fe0ebe67e791b817737cdc0d4f941764d7a114
Javier Montalt Tordera <javier.montalt@outlook.com>
2021-11-08 11:23:08 +0000

    Bump TF version to 2.7

--- setup.py
+++ setup.py
@@ -71,7 +71,6 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: C++',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
