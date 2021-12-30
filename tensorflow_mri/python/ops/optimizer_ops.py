a7d7871b6012bcbaad3dab00deea819895b3cdad
Javier Montalt Tordera <javier.montalt@outlook.com>
2021-12-30 17:48:40 +0100

    ADMM work in progress

--- tensorflow_mri/python/ops/optimizer_ops.py
+++ tensorflow_mri/python/ops/optimizer_ops.py
@@ -17,7 +17,55 @@
 This module contains solvers for nonlinear optimization problems.
 """
 
+import collections
+
+import tensorflow as tf
 import tensorflow_probability as tfp
+
+
+AdmmOptimizerResults = collections.namedtuple(
+    'AdmmOptimizerResults', [
+        'num_iterations'  # The number of iterations of the ADMM update.
+    ]
+)
+
+
+def admm_minimize(function_f, function_g, tolerance=1e-8, max_iterations=50):
+  """Applies the ADMM algorithm to minimize `f + g`.
+
+  Minimizes :math:`f(x) + g(z)`, subject to :math:`Ax + Bz = c`.
+
+  If :math:`A`, :math:`B` and :math:`c` are not provided, the constraint
+  defaults to :math:`x - z = 0`, in which case the problem is equivalent to
+  minimizing :math:`f(x) + g(x)`.
+
+  Args:
+    function_f: A `ConvexFunction`.
+    function_g: A `ConvexFunction`.
+    tolerance: A `float`. Specifies the tolerance of the optimization.
+    max_iterations: An `int`. The maximum number of iterations for ADMM updates.
+  """
+  dtype = tf.dtypes.float32
+
+  tolerance = tf.convert_to_tensor(
+      tolerance, dtype=dtype.real_dtype, name='tolerance')
+  max_iterations = tf.convert_to_tensor(
+      max_iterations, dtype=tf.dtypes.int32, name='max_iterations')
+
+  def _cond(state):
+    """Returns `True` if optimization should continue."""
+    return state.num_iterations < max_iterations
+
+  def _body(state):
+    """A single ADMM step."""
+    pass
+
+  # Initial state.
+  state = AdmmOptimizerResults(
+      num_iterations=tf.constant(0, dtype=tf.dtypes.int32))
+
+  return tf.while_loop(_cond, _body, [state])
+
 
 
 lbfgs_minimize = tfp.optimizer.lbfgs_minimize
