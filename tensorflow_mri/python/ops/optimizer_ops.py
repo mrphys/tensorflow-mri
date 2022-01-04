1c69bba74980df9ef314b16abd9a99a2ae4a3b67
Javier Montalt Tordera <javier.montalt@outlook.com>
2022-01-04 11:58:24 +0100

    ADMM work in progress

--- tensorflow_mri/python/ops/optimizer_ops.py
+++ tensorflow_mri/python/ops/optimizer_ops.py
@@ -25,12 +25,22 @@ import tensorflow_probability as tfp
 
 AdmmOptimizerResults = collections.namedtuple(
     'AdmmOptimizerResults', [
-        'num_iterations'  # The number of iterations of the ADMM update.
+        'i',  # The number of iterations of the ADMM update.
+        'x',  # The first primal variable.
+        'z',  # The second primal variable.
+        'u',  # The scaled dual variable.
+        'r',  # The primal residual.
+        's',  # The dual residual.
+        'primal_tolerance',   # The primal tolerance.
+        'dual_tolerance'      # The dual tolerance.
     ]
 )
 
 
-def admm_minimize(function_f, function_g, tolerance=1e-8, max_iterations=50):
+def admm_minimize(function_f, function_g,
+                  operator_a=None, operator_b=None, constant_c=None,
+                  rho=1.0, abs_tolerance=1e-5, rel_tolerance=1e-5,
+                  max_iterations=50):
   """Applies the ADMM algorithm to minimize `f + g`.
 
   Minimizes :math:`f(x) + g(z)`, subject to :math:`Ax + Bz = c`.
@@ -42,27 +52,91 @@ def admm_minimize(function_f, function_g, tolerance=1e-8, max_iterations=50):
   Args:
     function_f: A `ConvexFunction`.
     function_g: A `ConvexFunction`.
-    tolerance: A `float`. Specifies the tolerance of the optimization.
-    max_iterations: An `int`. The maximum number of iterations for ADMM updates.
+    operator_a: A `LinearOperator`. Defaults to the identity operator.
+    operator_b: A `LinearOperator`. Defaults to the negative identity operator.
+    constant_c: A scalar `Tensor`. Defaults to 0.0.
+    rho: A scalar `Tensor`. The augmented Lagrangian parameter. The step size
+      of the dual variable update in the scaled form of ADMM.
+    abs_tolerance: A scalar `Tensor`. The absolute tolerance. Defaults to 1e-5.
+    rel_tolerance: A scalar `Tensor`. The relative tolerance. Defaults to 1e-5.
+    max_iterations: A scalar `Tensor`. The maximum number of iterations for ADMM
+      updates.
   """
   dtype = tf.dtypes.float32
+  shape = (1,)
 
-  tolerance = tf.convert_to_tensor(
-      tolerance, dtype=dtype.real_dtype, name='tolerance')
+  abs_tolerance = tf.convert_to_tensor(
+      abs_tolerance, dtype=dtype.real_dtype, name='abs_tolerance')
+  rel_tolerance = tf.convert_to_tensor(
+      rel_tolerance, dtype=dtype.real_dtype, name='rel_tolerance')
   max_iterations = tf.convert_to_tensor(
       max_iterations, dtype=tf.dtypes.int32, name='max_iterations')
 
   def _cond(state):
     """Returns `True` if optimization should continue."""
-    return state.num_iterations < max_iterations
+    # Check tolerance condition.
+    r_norm = tf.norm(state.r, axis=-1)
+    s_norm = tf.norm(state.s, axis=-1)
+    tol_condition = tf.math.logical_and(r_norm > state.primal_tolerance,
+                                        s_norm > state.dual_tolerance)
+
+    # Check global condition with maximum of iterations.
+    iter_condition = tf.math.less(state.i, max_iterations)
+    return tf.math.logical_and(tol_condition, iter_condition)
 
   def _body(state):
     """A single ADMM step."""
-    pass
+    # x-minimization step.
+    state_bz = tf.linalg.matvec(operator_b, state.z)
+    x = function_f.prox(
+        tf.linalg.matvec(
+            operator_a, state_bz - state.u + constant_c,  # TODO: check
+            adjoint_a=True))
 
+    # z-minimization step.
+    ax = tf.linalg.matvec(operator_a, x)
+    z = function_g.prox(
+        tf.linalg.matvec(
+            operator_b, ax + state.u - constant_c,  # TODO: check
+            adjoint_a=False))
+
+    # Dual variable update.
+    bz = tf.linalg.matvec(operator_b, z)
+    r = ax + bz - constant_c  # TODO: check
+    u = state.u + r
+    s = rho * tf.norm(tf.linalg.matvec(operator_a, bz - state_bz, adjoint_a=True), axis=-1)
+
+    # Shape of primal and dual variables.
+    n = state.x.shape[-1]
+    p = state.u.shape[-1]
+
+    # Choose the primal tolerance.
+    ax_norm = tf.norm(ax, axis=-1)
+    bz_norm = tf.norm(bz, axis=-1)
+    c_norm = tf.norm(constant_c, axis=-1)
+    max_norm = tf.math.maximum(tf.math.maximum(ax_norm, bz_norm), c_norm)
+    primal_tolerance = (abs_tolerance * tf.math.sqrt(p) +
+                        rel_tolerance * max_norm)
+
+    # Choose the dual tolerance. 
+    aty_norm = tf.math.norm(
+        tf.linalg.matvec(operator_a, rho * state.u, adjoint_a=True), axis=-1)
+    dual_tolerance = (abs_tolerance * tf.math.sqrt(n) +
+                      rel_tolerance * aty_norm)
+
+    return AdmmOptimizerResults(i=state.i + 1,
+                                x=x,
+                                z=z,
+                                u=u,
+                                r=r,
+                                s=s,
+                                primal_tolerance=primal_tolerance,
+                                dual_tolerance=dual_tolerance)
   # Initial state.
   state = AdmmOptimizerResults(
-      num_iterations=tf.constant(0, dtype=tf.dtypes.int32))
+      i=tf.constant(0, dtype=tf.dtypes.int32),
+      z=tf.constant(0.0, dtype=dtype, shape=shape),
+      u=tf.constant(0.0, dtype=dtype, shape=shape))
 
   return tf.while_loop(_cond, _body, [state])
 
