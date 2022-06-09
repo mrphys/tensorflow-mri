e19eb2533cae9e02c270e3834142c3851498625e
Javier Montalt Tordera <javier.montalt@outlook.com>
2022-06-09 15:16:08 +0000

    Update API

--- tensorflow_mri/_api/models/__init__.py
+++ tensorflow_mri/_api/models/__init__.py
@@ -2,6 +2,9 @@
 # Do not edit.
 """Keras models."""
 
+from tensorflow_mri.python.models.conv_blocks import ConvBlock1D as ConvBlock1D
+from tensorflow_mri.python.models.conv_blocks import ConvBlock2D as ConvBlock2D
+from tensorflow_mri.python.models.conv_blocks import ConvBlock3D as ConvBlock3D
 from tensorflow_mri.python.models.conv_endec import UNet1D as UNet1D
 from tensorflow_mri.python.models.conv_endec import UNet2D as UNet2D
 from tensorflow_mri.python.models.conv_endec import UNet3D as UNet3D
