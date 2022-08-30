736cd165bde5ff6f9993cc764c4b992a5a714a6b
Javier Montalt Tordera <javier.montalt@outlook.com>
2022-08-30 13:07:13 +0000

    Updated copyright notice for FFT kernels

--- tensorflow_mri/cc/kernels/fft_kernels.cc
+++ tensorflow_mri/cc/kernels/fft_kernels.cc
@@ -1,3 +1,18 @@
+/* Copyright 2022 The TensorFlow MRI Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
 /* Copyright 2015 The TensorFlow Authors. All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,6 +27,9 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
+// This file is inspired by "tensorflow/tensorflow/core/kernels/fft_ops.cc",
+// but CPU kernels have been modified to use the FFTW library. The original
+// GPU kernels have been removed.
 
 #include "tensorflow/core/platform/errors.h"
 #define EIGEN_USE_THREADS
